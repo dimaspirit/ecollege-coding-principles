3a40d61e7ae5611f35c66200bb4cc273c77c84de
Dmytro Lobanov <dspwww@gmail.com>
2025-05-30 18:21:23 +0100

    A5-EX1: Add luckyNumber

---
+++ luckyNumber.py
@@ -0,0 +1,5 @@
+# Create a Python script ('luckyNumber.py') that generates a random number between 1 and 10 (inclusive).
+# Print the random number to the console.
+import random
+
+print('Random number generated from 1 - 10:', random.randint(1, 10))
