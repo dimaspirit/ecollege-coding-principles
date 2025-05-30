34f0b5eba9945b2f83219734009de46433718503
Dmytro Lobanov <dspwww@gmail.com>
2025-05-30 18:24:12 +0100

    A5-E2: Add exoticFruitSelector

---
+++ exoticFruitSelector.py
@@ -0,0 +1,5 @@
+import random
+
+fruits = ("Banana", "Pineapple", "Jackfruit", "Mangoes", "Avocado", "Coconut")
+
+print("The random tropical fruit selected from the list is", random.choice(fruits))
